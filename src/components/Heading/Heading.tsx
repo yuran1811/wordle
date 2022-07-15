1eb87735b0fb8ac288149c8a6c2c378d803d939b
yuran1811 <trieuvanbd123@gmail.com>
2022-07-15 23:22:54 +0700

    deploy

--- src/components/Heading/Heading.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC, PropsWithChildren } from 'react';
-
-export const Heading: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="font-bold text-[4rem] text-center text-white p-4">{children}</div>;
-};
--- src/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import boardReducer from './boardSlice';
-
-export default configureStore({
-  reducer: {
-    board: boardReducer,
-  },
-});
